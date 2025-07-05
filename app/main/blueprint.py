1fbb5df5434ea962cb9f503cb6325421627e06ad
Maffi <160107114+maffiiiii@users.noreply.github.com>
2025-07-05 11:53:52 +0200

    solved routation and added some more adequat style

--- app/main/blueprint.py
+++ app/main/blueprint.py
@@ -2,5 +2,5 @@ from flask import Blueprint, render_template
 
 main_bp = Blueprint("main", __name__)
 
-notes_bp = Blueprint("notes", import_name=__name__,)
+notes_bp = Blueprint("notes", import_name=__name__, template_folder="templates")  #i added tempalates cause i didnt find a html file
 
